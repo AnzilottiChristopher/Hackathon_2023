e7fbe7e509f15a266af302ec8abecda663b6f87d
Chris Anzilotti <anzilottichr@gmail.com>
2023-11-04 12:34:27 -0400

    Screens Switching

--- src/RiddleScreen.java
+++ src/RiddleScreen.java
@@ -5,54 +5,77 @@ import java.awt.event.ActionListener;
 
 public class RiddleScreen
 {
-    private JFrame frame;
-    private JPanel panel;
+    private JFrame mainFrame;
+    private JPanel startPanel;
     private CardLayout cardLayout;
+    private JPanel cardPanel;
     public void initialize() {
-        frame = new JFrame("Riddle Screen");
-        panel = new JPanel();
-        cardLayout = new CardLayout();
+        //Main Frame
+        mainFrame = new JFrame("Riddle Screen");
 
+        //Starting Panel
+        startPanel = new JPanel();
+
+
+
+        //Start messages
         JLabel welcomeMessage = new JLabel("Welcome to the riddle portion!");
         JButton nextButton = new JButton("Next");
         GridBagConstraints gbc = new GridBagConstraints();
-        panel.setLayout(new GridBagLayout());
+        startPanel.setLayout(new GridBagLayout());
+
+        //Moving things to center
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        startPanel.add(nextButton, gbc);
 
 
-        gbc.gridx = GridBagConstraints.CENTER;
-        gbc.gridy = GridBagConstraints.CENTER;
-        panel.add(nextButton, gbc);
-
-
-        gbc.gridx = GridBagConstraints.CENTER;
+        gbc.gridx = 0;
         gbc.gridy = 0;
-        panel.add(welcomeMessage, gbc);
+        startPanel.add(welcomeMessage, gbc);
 
 
+        //Creating Seperate panel
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        cardPanel.add(startPanel, "Card1");
+
+        JPanel card2 = new JPanel();
+        card2.add(new JLabel("Card 2 Content"));
+        cardPanel.add(card2, "Card2");
+
+        //Listening for button press and changing screen
         nextButton.addActionListener(e ->
         {
-            panel.removeAll();
-            panel.revalidate();
-            panel.repaint();
+            startPanel.removeAll();
+            startPanel.revalidate();
+            startPanel.repaint();
+            cardLayout.next(cardPanel);
         });
 
-        // Use null layout
-        //frame.setLayout(null);
+
 
         // Adjust the button's location to be within the visible area
 
+        //Adjusting font
         Font largerFont = new Font(welcomeMessage.getFont().getName(), Font.PLAIN, 24);
         welcomeMessage.setFont(largerFont);
 
-        frame.add(panel);        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
+        mainFrame.add(cardPanel);
+
+        cardLayout.show(cardPanel, "Card1");
+
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setSize(800, 800);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int centerX = (int) (screenSize.getWidth() - frame.getWidth()) / 2;
-        int centerY = (int) (screenSize.getHeight() - frame.getHeight()) / 2;
-        frame.setLocation(centerX, centerY);
+        int centerX = (int) (screenSize.getWidth() - mainFrame.getWidth()) / 2;
+        int centerY = (int) (screenSize.getHeight() - mainFrame.getHeight()) / 2;
+        mainFrame.setLocation(centerX, centerY);
 
-        frame.setVisible(true);
+        mainFrame.setVisible(true);
 
     }
 
