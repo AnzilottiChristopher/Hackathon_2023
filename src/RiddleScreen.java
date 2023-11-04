03e4b4eec19f622a5c82f5842d38c5f327a1d286
Chris Anzilotti <anzilottichr@gmail.com>
2023-11-04 12:18:02 -0400

    Buttons

--- src/RiddleScreen.java
+++ src/RiddleScreen.java
@@ -1,26 +1,62 @@
 import javax.swing.*;
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RiddleScreen
 {
-    public void initialize()
-    {
-        JFrame frame = new JFrame("Riddle Screen");
-        JPanel panel = new JPanel();
+    private JFrame frame;
+    private JPanel panel;
+    private CardLayout cardLayout;
+    public void initialize() {
+        frame = new JFrame("Riddle Screen");
+        panel = new JPanel();
+        cardLayout = new CardLayout();
 
-        frame.add(panel);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JLabel welcomeMessage = new JLabel("Welcome to the riddle portion!");
+        JButton nextButton = new JButton("Next");
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(new GridBagLayout());
 
+
+        gbc.gridx = GridBagConstraints.CENTER;
+        gbc.gridy = GridBagConstraints.CENTER;
+        panel.add(nextButton, gbc);
+
+
+        gbc.gridx = GridBagConstraints.CENTER;
+        gbc.gridy = 0;
+        panel.add(welcomeMessage, gbc);
+
+
+        nextButton.addActionListener(e ->
+        {
+            panel.removeAll();
+            panel.revalidate();
+            panel.repaint();
+        });
+
+        // Use null layout
+        //frame.setLayout(null);
+
+        // Adjust the button's location to be within the visible area
+
+        Font largerFont = new Font(welcomeMessage.getFont().getName(), Font.PLAIN, 24);
+        welcomeMessage.setFont(largerFont);
+
+        frame.add(panel);        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
         int centerX = (int) (screenSize.getWidth() - frame.getWidth()) / 2;
         int centerY = (int) (screenSize.getHeight() - frame.getHeight()) / 2;
-
         frame.setLocation(centerX, centerY);
 
         frame.setVisible(true);
 
     }
+
+
+
+
 }
